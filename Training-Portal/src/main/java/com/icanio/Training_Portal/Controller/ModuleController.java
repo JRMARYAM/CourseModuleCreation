3b8ffbcff1b1e274e47ac4d61412f1d01367b69f
Arasu-j <arasuthedon03@gmail.com>
2024-08-12 11:20:51 +0530

    updated

--- Training-Portal/src/main/java/com/icanio/Training_Portal/Controller/ModuleController.java
+++ Training-Portal/src/main/java/com/icanio/Training_Portal/Controller/ModuleController.java
@@ -5,6 +5,7 @@ import com.icanio.Training_Portal.Entity.Module;
 import com.icanio.Training_Portal.Service.ModuleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,23 +13,24 @@ import java.util.Optional;
 
 
 @RestController
-@RequestMapping("/api/courses")
+@PreAuthorize("hasAuthority('USER')")
+@RequestMapping("/courses")
 public class ModuleController {
 
  @Autowired
  private ModuleService moduleService;
-
+ @PreAuthorize("hasAuthority('USER')")
  @GetMapping("/allcourses")
  public List<ModuleResponse> all() {
   return moduleService.all();
  }
-
+ @PreAuthorize("hasAuthority('USER')")
  @GetMapping("/get/{id}")
  public ResponseEntity<Module> getCourseById(@PathVariable Long id) {
   Optional<Module> course = moduleService.getCourseById(id);
   return course.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
  }
-
+ @PreAuthorize("hasAuthority('USER')")
  @PostMapping("/register")
  public ResponseEntity<Module> saveOrUpdateCourse(@RequestBody Module module) {
   Module savedModule = moduleService.saveOrUpdateCourse(module);
